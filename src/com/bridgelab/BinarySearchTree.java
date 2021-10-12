691745252c60904d2f64434e84f3ebe6365bc193
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-12 18:51:51 +0530

    Added : searching elements in BST and show its present or not

--- src/com/bridgelab/BinarySearchTree.java
+++ src/com/bridgelab/BinarySearchTree.java
@@ -18,5 +18,10 @@ public class BinarySearchTree {
 		myTree.add(63);
 		myTree.add(67);
 		myTree.print(myTree.root);
+		boolean flag = myTree.search(63);
+		if (flag == true)
+			System.out.println("\n63 is present");
+		else
+			System.out.println("\n63 is not present");
 	}
 }
--- src/com/bridgelab/BinaryTree.java
+++ src/com/bridgelab/BinaryTree.java
@@ -33,6 +33,7 @@ public class BinaryTree<K extends Comparable<K>> {
 		}
 	}
 
+	/* This Method to display the Binary Search Tree */
 	public void print(BinaryNode<K> node) {
 		if (node == null) {
 			System.out.println("Tree is empty");
@@ -44,5 +45,26 @@ public class BinaryTree<K extends Comparable<K>> {
 				print(node.right);
 
 		}
+	}
+
+	public BinaryNode<K> searchNode(BinaryNode<K> root, int key) {
+		// root is null or key is present at root
+		if (root == null || root.key == key)
+			return root;
+		// value is greater than key
+		if (root.key > key)
+			return searchNode(root.left, key);
+		// value is less than key
+		return searchNode(root.right, key);
+
+	}
+
+	public boolean search(int value) {
+		root = searchNode(root, value);
+		if (root != null)
+			return true;
+		else
+			return false;
+
 	}
 }
